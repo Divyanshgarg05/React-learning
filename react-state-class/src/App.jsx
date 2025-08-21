c033615e378182bd7c58149431d2832127d0067a
Divyansh Garg <divyanshgarg844@gmail.com>
2025-08-21 21:46:37 +0530

    Learned Callback

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -3,12 +3,12 @@ import LikeButton from './LikeButton';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <p>States in react</p>
-      <LikeButton/>
+      <Counter/>
     </>
   )
 }
